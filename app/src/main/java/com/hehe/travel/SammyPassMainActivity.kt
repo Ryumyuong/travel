13b1ccbcdc23af2285d5d112bd9faa4eb629f73a
shinji <tlswl7479@gmail.com>
2025-10-17 16:04:03 +0900

    Add sammy padd page1, 2

---
+++ app/src/main/java/com/hehe/travel/SammyPassMainActivity.kt
@@ -0,0 +1,45 @@
+package com.hehe.travel
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SammyPassMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sammy_pass_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // XML에서 설정한 패딩 값 유지하면서 시스템 바 insets 추가
+            val originalPaddingLeft = v.paddingLeft
+            val originalPaddingTop = v.paddingTop
+            val originalPaddingRight = v.paddingRight
+            val originalPaddingBottom = v.paddingBottom
+            v.setPadding(
+                originalPaddingLeft + systemBars.left,
+                originalPaddingTop + systemBars.top,
+                originalPaddingRight + systemBars.right,
+                originalPaddingBottom + systemBars.bottom
+            )
+            insets
+        }
+
+        val btnStart = findViewById<Button>(R.id.btnStart)
+        val btnBrowse = findViewById<Button>(R.id.btnBrowse)
+
+        btnStart.setOnClickListener {
+            // TODO: Navigate to Sammy Pass questionnaire
+            Toast.makeText(this, "작성 시작하기", Toast.LENGTH_SHORT).show()
+        }
+
+        btnBrowse.setOnClickListener {
+            // TODO: Navigate to main app without completing Sammy Pass
+            Toast.makeText(this, "둘러보기 모드", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/hehe/travel/SammyQuestionActivity.kt
@@ -0,0 +1,30 @@
+package com.hehe.travel
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SammyQuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sammy_question)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // XML에서 설정한 패딩 값 유지하면서 시스템 바 insets 추가
+            val originalPaddingLeft = v.paddingLeft
+            val originalPaddingTop = v.paddingTop
+            val originalPaddingRight = v.paddingRight
+            val originalPaddingBottom = v.paddingBottom
+            v.setPadding(
+                originalPaddingLeft + systemBars.left,
+                originalPaddingTop + systemBars.top,
+                originalPaddingRight + systemBars.right,
+                originalPaddingBottom + systemBars.bottom
+            )
+            insets
+        }
+    }
+}
