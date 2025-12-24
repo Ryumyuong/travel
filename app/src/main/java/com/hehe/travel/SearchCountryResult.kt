023cc6a19f00edb32738753a55b0785b6300c8aa
tlswl7479 <tlswl7479@gmail.com>
2025-12-24 11:20:34 +0900

    Fix: fix design

--- app/src/main/java/com/hehe/travel/SearchCountryResult.kt
+++ app/src/main/java/com/hehe/travel/SearchCountryResult.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class plan_recommended_with_sammy_pass : AppCompatActivity() {
+class SearchCountryResult : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_plan_recommended_with_sammy_pass)
+        setContentView(R.layout.activity_search_country_result)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
